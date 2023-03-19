2da2fef2d04dd9fc0efc0cae5d8657af7b09cf70
Trevor Keegan <44409350+inquisitev@users.noreply.github.com>
2023-03-18 19:39:08 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Trevor  Keegan on 3/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
