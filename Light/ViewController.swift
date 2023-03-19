bab0bd9a0ccd88c69bf8eb650552946c0687513c
Trevor Keegan <44409350+inquisitev@users.noreply.github.com>
2023-03-18 20:14:17 -0500

    Add button to toggle background

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isLit = false
 
+    @IBAction func lightButton(_ sender: Any) {
+        isLit = !isLit
+        
+        if(isLit)
+        {
+            view.backgroundColor = .black
+        }
+        else
+        {
+            view.backgroundColor = .white
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
